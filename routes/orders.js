4be10d7d944734335ad6bbec865d820040f4693d
Thang Do <18thangd@gmail.com>
2020-08-29 18:32:56 +0700

    Update orders route

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,13 @@
 const { Order, validateOrder, validateOrderProductAddition } = require('../models/Order')
 const { User } = require('../models/User')
 const { Product } = require('../models/Product')
+const { OrderProduct } = require('../models/OrderProduct')
+const { Category } = require('../models/Category')
 const sequelize = require('../db/connection')
 const express = require('express')
 const router = express.Router()
 
+// get all orders
 router.get('/', async (req, res) => {
   const orders = await Order.findAll({ attributes: {
     exclude: ['createdAt', 'updatedAt']
@@ -12,6 +15,7 @@ router.get('/', async (req, res) => {
   return res.json({ status: 200, data: orders })
 })
 
+// get order by specify id
 router.get('/:id', async (req, res) => {
   const order = await Order.findOne({ 
     where: { id: req.params.id },
@@ -29,15 +33,16 @@ router.get('/:id', async (req, res) => {
     attributes: { exclude: ['createdAt', 'updatedAt' ]}
   })
   if (!order) return res.json({ status: 404, message: 'Order not found' })
-  console.log(order.OrderProduct)
   return res.json({ status: 200, data: order })
 })
 
+// create an order without any product
 router.post('/', [validateOrder], async (req, res) => {
   const order = await Order.create(req.body)
   return res.json({ status: 201, data: order })
 })
 
+// add a product for order
 router.post('/newProduct', [validateOrderProductAddition], async (req, res) => {
   const order = await Order.findOne({ where: { id: req.body.orderId }})
   if (!order) return res.json({ status: 404, message: 'Order not found' })
@@ -71,29 +76,71 @@ router.post('/newProduct', [validateOrderProductAddition], async (req, res) => {
   }
 })
 
-router.post('/:id', async (req, res) => {
-  const order = await Order.findOne({ where: { id: req.body.id }})
-  if (!order) return res.json({ status: 404, message: 'Order not found' })
-
-  const user = await User.findOne({ where: { id: req.params.id }})
-  if (!user) return res.json({ status: 404, message: 'User not found' })
-
-  order.addUser(order.id, user.id)
-  return res.json({ status: 200 })
+// edit a product inside of an order (edit productAmount of product)
+router.patch('/product', [validateOrderProductAddition], async (req, res) => {
+  // check for existence of Order & OrderProduct
+  const orderProduct = await OrderProduct.findOne({ where: {
+    productId: req.body.productId,
+    orderId: req.body.orderId
+  }})
+  if (!orderProduct) res.json({
+    status: 404,
+    message: 'Order not found or Order does not contain that Product'
+  })
+  // take product currently in the store
+  const storeProduct = await Product.findOne({ where: { id: req.body.productId }})
+  // calculate total product amount of that product
+  const totalStoreProductAmount = orderProduct.productAmount + storeProduct.numberInStock
+  // check for stocks
+  if (totalStoreProductAmount < req.body.productAmount) return res.json({
+    status: 400,
+    message: `Product ${storeProduct.name} is currently not having enough stock`
+  })
+  // create transaction for update order & store product amount
+  const transaction = await sequelize.transaction()
+  try {
+    await orderProduct.update({ productAmount: req.body.productAmount }, { transaction: transaction })
+    await storeProduct.update({ numberInStock: (totalStoreProductAmount - req.body.productAmount) }, { transaction: transaction })
+  }
+  catch(err) {
+    console.log(err)
+    await transaction.rollback()
+    return res.json({ status: 404, message: `Transaction failed: ${err.message}` })
+  }
+  // take order to response
+  const order = await Order.findOne({ 
+    where: { id: req.body.orderId },
+    include: {
+      model: Product,
+      as: 'products',
+      where: { id: req.body.productId },
+      attributes: { exclude: ['createdAt', 'updatedAt', 'categoryId'] },
+      through: { attributes: ['id', 'productAmount'] },
+      include: {
+        model: Category,
+        as: 'category',
+        attributes: { exclude: ['createdAt', 'updatedAt'] }
+      }
+    }
+  })
+  // commit transaction & response
+  await transaction.commit()
+  return res.json({ status: 200, data: await order.reload() })
 })
 
-// router.delete('/:id', async (req, res) => {
-//   const deletedCount = await Order.destroy({ where: { id: req.params.id }})
-//   return res.json({ status: 200, deletedCount })
-// })
+// edit order, find order by specify id
+router.patch('/:id', [validateOrder], async (req, res) => {
+  const order = await Order.findOne({ where: { id: req.params.id }})
+  if (!order) return res.json({ status: 404, message: 'Order not found' })
 
-// router.delete('/', async (req, res) => {
-//   if (!req.query.phone && !req.query.email) throw new Error('There\'s no phone or email provided')
-//   // define option to delete
-//   const option = (req.query.email) ? { email: req.query.email } : { phone: req.query.phone }
-//   // delete 
-//   const deletedCount = await Order.destroy({ where: option })
-//   return res.json({ status: 200, deletedCount })
-// })
+  const updatedOrder = await order.update(req.body)
+  return res.json({ status: 200, data: updatedOrder })
+})
+
+// delete an order by id
+router.delete('/:id', async (req, res) => {
+  const deletedCount = await Order.destroy({ where: { id: req.params.id }})
+  return res.json({ status: 200, deletedCount })
+})
 
 module.exports = router
